9f67cee87f18ca509829d2515195fd6ac50e5e58
200651 <200651@e0601-23.JC-21.local>
2022-01-31 11:29:00 +0900

    OtoKadaiの提出

---
+++ src/OtoKadai02.java
@@ -0,0 +1,33 @@
+import jp.jc21.t.yoshizawa.otopika.Otopika;
+
+/**
+ *
+ */
+
+
+/**
+ * @author AB2B03
+ *
+ */
+public class OtoKadai02  {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		Otopika oto = new Otopika();
+		oto.powerOn();
+		while(true) {
+			oto.lightGreen();
+			oto.sleep();
+			oto.lightYellow();
+			oto.sleep();
+			oto.lightRed();
+			oto.sleep();
+			oto.lightOff();
+			oto.sleep();
+		}
+
+	}
+
+}
