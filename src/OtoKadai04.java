c03635a7905b560b5a4e1f3974d5c7943bd73953
200651 <200651@e0601-23.JC-21.local>
2022-02-03 12:11:57 +0900

    修正版

--- src/OtoKadai04.java
+++ src/OtoKadai04.java
@@ -23,12 +23,11 @@ public class OtoKadai04  {
 		while(true) {
 			oto1.lightAll();
 			oto2.lightOff();
-			oto1.sleep();
 			oto2.sleep();
 			oto1.lightOff();
 			oto2.lightAll();
 			oto1.sleep();
-			 oto2.sleep();
+			
 			
 		
 
